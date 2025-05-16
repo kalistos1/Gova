607614a58ac1c03efdf566498054686186f47622
kalistos1 <ucktech1@gmail.com>
2025-05-16 04:57:05 +0100

    disablede

--- gova/settings.py
+++ gova/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'django.contrib.gis',
+    # 'django.contrib.gis',
     
     # Third-party apps
    
